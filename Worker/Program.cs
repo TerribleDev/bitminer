8d738fe39d66072e6866ce8ac3f0128bb3ae357f
Tommy Parnell <tommy@terribledev.io>
2017-01-11 21:29:21 -0500

    add worker df

--- Worker/Program.cs
+++ Worker/Program.cs
@@ -17,12 +17,19 @@ namespace Worker
         {
             while(true)
             {
-                var bytes = await httpclient.GetByteArrayAsync("http://gen/8");
-                var results = await httpclient.PostAsync("http://hashr/hashme",  new ByteArrayContent(bytes));
-                results.EnsureSuccessStatusCode();
-                var hashResults = await results.Content.ReadAsStringAsync();
-                var dbResult = await httpclient.GetStringAsync($"http://store/store?dt={DateTime.Now.ToString()}");
-                await Task.Delay(1000);
+                try
+                {
+                    var bytes = await httpclient.GetByteArrayAsync("http://gen/8");
+                    var results = await httpclient.PostAsync("http://hashr/hashme",  new ByteArrayContent(bytes));
+                    results.EnsureSuccessStatusCode();
+                    var hashResults = await results.Content.ReadAsStringAsync();
+                    var dbResult = await httpclient.GetStringAsync($"http://store/store?dt={DateTime.Now.ToString()}");
+                    await Task.Delay(1000);
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
             }
         }
     }
