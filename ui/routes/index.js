9d981341c04e5a986fb1211f547de5e85e771232
Tommy Parnell <tparnell8@gmail.com>
2017-01-11 22:15:11 -0500

    more df, ui works

--- ui/routes/index.js
+++ ui/routes/index.js
@@ -1,10 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 /* GET home page. */
 
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/db', (req, res)=>{
+    // request('http://store/', function (error, response, body) {
+    // if (!error && response.statusCode == 200) {
+    //     console.log(body) // Show the HTML for the Google homepage.
+    //   }
+    //   res.set('Content-Type', 'application/json');
+    //   return res.send(200, body);
+    // })
+    res.json([ { date: '2017-01-01 10:01', value: 20 }, { date: '2017-01-01 11:00', value: 60 }]);
 });
 
 module.exports = router;
